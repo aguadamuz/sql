2c2598e91030f382c16f97130949cbc08247b8f9
aguadamuz <aguadamuz@change.org>
2016-01-29 13:28:29 -0800

    updated zendesk query to include ticket count by user_id

--- zendesk_ua001.sql
+++ zendesk_ua001.sql
@@ -2,14 +2,24 @@
 
 
 --pull change.org user ids for the zendesk users based on their email address
+--pull change.org user ids for the zendesk users based on their email address
+--pull change.org user ids for the zendesk users based on their email address
+WITH ua_users AS
+(
 WITH zendesk_users AS
 (
 SELECT ozu.email, ozu.id AS zendesk_id, u.id, u.first_name, u.last_name, datediff(year, u.birthday, current_date) AS age, u.country, u.locale
 FROM olga.zendesk_users ozu JOIN users u ON ozu.email = u.email
 )
-SELECT DISTINCT zu.email, zu.zendesk_id, zu.id, zu.first_name, zu.last_name, zu.age, zu.country, u.locale
+SELECT DISTINCT zu.email, zu.zendesk_id, zu.id, zu.first_name, zu.last_name, zu.age, zu.country, zu.locale
 FROM zendesk_users zu
-LIMIT 300000;
+)
+SELECT uau.email, uau.zendesk_id, uau.id, uau.first_name, uau.last_name, uau.age, uau.country, uau.locale, count(distinct ozt.nice_id)
+FROM ua_users uau
+JOIN olga.zendesk_tickets ozt ON uau.zendesk_id = ozt.requester_id
+GROUP BY 1,2,3,4,5,6,7,8
+ORDER BY 9 DESC
+LIMIT 999;
 
 
 
