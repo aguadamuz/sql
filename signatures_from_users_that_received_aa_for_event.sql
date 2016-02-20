aae52e705c15c6bb2e2c0574a64e37e743ac03f6
aguadamuz <aguadamuz@change.org>
2016-02-19 16:30:42 -0800

    initial commit

---
+++ signatures_from_users_that_received_aa_for_event.sql
@@ -0,0 +1,17 @@
+WITH aa AS
+(
+SELECT DISTINCT
+aaue.user_id
+FROM action_alert_user_events aaue
+JOIN action_alert_records aar ON aaue.action_alert_record_id = aar.id
+JOIN action_alert_contents aac ON aar.action_alert_content_id = aac.id
+JOIN scheduled_action_alerts saa ON aac.scheduled_action_alert_id = saa.id
+JOIN campaign_teams ct ON saa.campaign_team_id = ct.id
+WHERE aaue.event = 'new' -- new = sent
+AND aac.event_id = 4991122
+)
+SELECT DISTINCT sp.user_id
+FROM signatures_petitions sp
+WHERE sp.petition_id = 4692014
+AND sp.country_code = 'US'
+AND sp.user_id IN (SELECT * FROM aa)
